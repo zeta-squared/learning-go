6eaf85d609850f225af75b05f7004833a1762a9b
zeta-squared <zeaiter.zeaiter@hotmail.com>
2025-07-29 20:36:26 +1000

    feat: added partial progress through chapter 9 exercises

---
+++ ch9/ex3/main.go
@@ -0,0 +1,122 @@
+package main
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"regexp"
+	"strings"
+)
+
+var ErrInvalidID = errors.New("invalid id")
+
+func main() {
+	d := json.NewDecoder(strings.NewReader(data))
+	count := 0
+	for d.More() {
+		count++
+		var emp Employee
+		err := d.Decode(&emp)
+		if err != nil {
+			fmt.Printf("record %d: %v\n", count, err)
+			continue
+		}
+
+		err = ValidateEmployee(emp)
+		if err != nil {
+			var emptyFieldErr EmptyFieldErr
+			if errors.Is(err, ErrInvalidID) {
+				fmt.Printf("record %d: %+v error: invalid ID: %s\n", count, emp, emp.ID)
+			} else if errors.As(err, &emptyFieldErr) {
+				fmt.Printf("record %d: %+v error: %v\n", count, emp, err)
+			} else {
+				fmt.Printf("record %d: %+v error: %v\n", count, emp, err)
+			}
+		}
+
+		fmt.Printf("record %d: %+v\n", count, emp)
+	}
+}
+
+const data = `
+{
+	"id": "ABCD-123",
+	"first_name": "Bob",
+	"last_name": "Bobson",
+	"title": "Senior Manager"
+}
+{
+	"id": "XYZ-123",
+	"first_name": "Mary",
+	"last_name": "Maryson",
+	"title": "Vice President"
+}
+{
+	"id": "BOTX-263",
+	"first_name": "",
+	"last_name": "Garciason",
+	"title": "Manager"
+}
+{
+	"id": "HLXO-829",
+	"first_name": "Pierre",
+	"last_name": "",
+	"title": "Intern"
+}
+{
+	"id": "MOXW-821",
+	"first_name": "Franklin",
+	"last_name": "Watanabe",
+	"title": ""
+}
+{
+	"id": "",
+	"first_name": "Shelly",
+	"last_name": "Shellson",
+	"title": "CEO"
+}
+{
+	"id": "YDOD-324",
+	"first_name": "",
+	"last_name": "",
+	"title": ""
+}
+`
+
+type Employee struct {
+	ID        string `json:"id"`
+	FirstName string `json:"first_name"`
+	LastName  string `json:"last_name"`
+	Title     string `json:"title"`
+}
+
+type EmptyFieldErr struct {
+	field string
+}
+
+func (err EmptyFieldErr) Error() string {
+	return fmt.Sprintf("empty field: %s", err.field)
+}
+
+var (
+	validID = regexp.MustCompile(`\W{4}-\d{3}`)
+)
+
+func ValidateEmployee(e Employee) error {
+	if len(e.ID) == 0 {
+		return EmptyFieldErr{field: "ID"}
+	}
+	if !validID.MatchString(e.ID) {
+		return ErrInvalidID
+	}
+	if len(e.FirstName) == 0 {
+		return EmptyFieldErr{field: "FirstName"}
+	}
+	if len(e.LastName) == 0 {
+		return EmptyFieldErr{field: "LastName"}
+	}
+	if len(e.Title) == 0 {
+		return EmptyFieldErr{field: "Title"}
+	}
+	return nil
+}

e9197dd80bfccd7cddf817eee2199c8610ad40ab
Zeaiter Zeaiter <zeaiter.zeaiter@zurich.com>
2025-08-15 10:01:48 +1000

    feat: added ch10

---
+++ ch10/workspace/workspace_mod/go.mod
@@ -0,0 +1,3 @@
+module github.com/zeta-squared/workspace_mod
+
+go 1.24.3
---
+++ ch10/workspace/workspace_mod/my_math.go
@@ -0,0 +1,11 @@
+package workspace_mod
+
+// Add is a function to perform addition.
+//
+// It takes two parameters, both of type int. It returns their sum as type int.
+// For more details on addition click [here].
+//
+// [here]: https://www.mathisfun.com/numbers/addition.html
+func Add(x, y int) int {
+	return x + y
+}

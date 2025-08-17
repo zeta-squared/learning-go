255525921cb75caa8c2d62c637f3839d77bc28fa
zeta-squared <zeaiter.zeaiter@hotmail.com>
2025-08-17 19:03:28 +1000

    chore: finished ch10 exercises

--- ch10/workspace/workspace_mod/go.mod
+++ ch10/workspace/workspace_mod/go.mod
@@ -1,3 +1,5 @@
 module github.com/zeta-squared/workspace_mod
 
 go 1.24.3
+
+require golang.org/x/exp v0.0.0-20250813145105-42675adae3e6
--- ch10/workspace/workspace_mod/my_math.go
+++ ch10/workspace/workspace_mod/my_math.go
@@ -1,11 +1,19 @@
 package workspace_mod
 
+import (
+	constraints "golang.org/x/exp/constraints"
+)
+
+type Number interface {
+	constraints.Integer | constraints.Float
+}
+
 // Add is a function to perform addition.
 //
 // It takes two parameters, both of type int. It returns their sum as type int.
 // For more details on addition click [here].
 //
 // [here]: https://www.mathisfun.com/numbers/addition.html
-func Add(x, y int) int {
+func Add[T Number](x, y T) T {
 	return x + y
 }

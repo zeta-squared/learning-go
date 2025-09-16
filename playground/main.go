e851a1cb27c32193b8e811145faa84136c8cbc8a
Zeaiter Zeaiter <zeaiter.zeaiter@zurich.com>
2025-09-17 07:16:41 +1000

    chore: playing with channels

--- playground/main.go
+++ playground/main.go
@@ -5,16 +5,21 @@ import (
 )
 
 func main() {
-	s := "abcabcbb"
-	fmt.Println(lengthOfLongestSubstring(s))
-	s = "bbbbb"
-	fmt.Println(lengthOfLongestSubstring(s))
-	s = "pwwkew"
-	fmt.Println(lengthOfLongestSubstring(s))
-	s = " "
-	fmt.Println(lengthOfLongestSubstring(s))
-	s = "au"
-	fmt.Println(lengthOfLongestSubstring(s))
-	s = "au"
-	fmt.Println(lengthOfLongestSubstring(s))
+	ch1 := make(chan int)
+	ch2 := make(chan int)
+	go func() {
+		inGoroutine := 1
+		ch1 <- inGoroutine
+		fromMain := <-ch2
+		fmt.Println("goroutine:", inGoroutine, fromMain)
+	}()
+	inMain := 2
+	var fromGoroutine int
+	select {
+	case ch2 <- inMain:
+	case fromGoroutine = <-ch1:
+	default:
+		fmt.Println("main thread first")
+	}
+	fmt.Println("main:", inMain, fromGoroutine)
 }

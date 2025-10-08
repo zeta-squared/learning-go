874d8737a598676736327302dfaccf5a4d8f4c54
zeta-squared <zeaiter.zeaiter@hotmail.com>
2025-10-09 06:34:51 +1100

    chore: leetcode working on merge sorted array problem

--- playground/main.go
+++ playground/main.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 type TreeNode struct {
 	Val   int
 	Left  *TreeNode
@@ -7,4 +9,28 @@ type TreeNode struct {
 }
 
 func main() {
+	x := []int{1, 2, 3, 0, 0, 0}
+	merge(x, []int{2, 5, 6}, 3, 3)
+	fmt.Println(x)
+}
+
+func merge(nums1, nums2 []int, m, n int) {
+	temp := make([]int, m+n)
+	i, j := 0, 0
+	for k := range len(nums1) {
+		if i > m {
+			temp[k] = nums2[j]
+			j++
+		} else if j > n {
+			temp[k] = nums1[i]
+			i++
+		}
+
+		temp[k] = min(nums1[i], nums2[j])
+		if nums1[i] <= nums2[j] {
+			i++
+		} else {
+			j++
+		}
+	}
 }
